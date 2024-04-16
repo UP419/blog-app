3c7b749261e5bdff6231cefa4d4739bf24e4dcb3
ucha <uchaucha419@gmail.com>
2024-04-16 18:55:23 +0400

    added blog delete button

--- src/BlogDetails.tsx
+++ src/BlogDetails.tsx
@@ -1,9 +1,20 @@
-import {useParams} from 'react-router-dom'
+import {useHistory, useParams} from 'react-router-dom'
 import useFetch from "./useFetch.tsx";
 
 const BlogDetails = () => {
     const {id} = useParams();
     const {data: blog, isPending, error} = useFetch("http://localhost:8000/blogs/" + id);
+
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + id, {
+            method: 'DELETE'
+        }).then(() => {
+            console.log("Deleted blog");
+            history.push('/');
+        })
+    }
 
     return (
         <div className="blog-details">
@@ -14,6 +25,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={() => handleClick()}>Delete blog</button>
                 </article>
             )}
         </div>
