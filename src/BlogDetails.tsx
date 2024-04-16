a6603361e0937487339e8cfa64a39a660e8f3bdf
ucha <uchaucha419@gmail.com>
2024-04-16 15:16:51 +0400

    reusing useFetch for blog pages

--- src/BlogDetails.tsx
+++ src/BlogDetails.tsx
@@ -1,11 +1,21 @@
 import {useParams} from 'react-router-dom'
+import useFetch from "./useFetch.tsx";
 
 const BlogDetails = () => {
     const {id} = useParams();
+    const {data: blog, isPending, error} = useFetch("http://localhost:8000/blogs/" + id);
 
     return (
-        <div>
-            <h1>Blog Details - {id}</h1>
+        <div className="blog-details">
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
     )
 }
